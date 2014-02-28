ab70ecea9045b928b646741975558ec8b0d54149
Joshua Hoblitt <jhoblitt@cpan.org>
2014-02-28 15:25:53 -0700

    yet another attempt to work around exec umask issues

--- manifests/add_cert_and_key.pp
+++ manifests/add_cert_and_key.pp
@@ -38,16 +38,28 @@ define nsstools::add_cert_and_key (
   # downcase and change spaces into _s
   $pkcs12_name = downcase(regsubst("${nickname}.p12", '[\s]', '_', 'GM'))
 
-  # the exec type in older versions of puppet don't support the umask param so
-  # we have to inline it in the command string
-  exec {"generate_pkcs12_${title}":
-    command   => "umask 7077 && /usr/bin/openssl pkcs12 -export -in ${cert} -inkey ${key} -password 'file:${certdir}/nss-password.txt' -out '${certdir}/${pkcs12_name}' -name '${nickname}'",
-    creates   => "${certdir}/${pkcs12_name}",
-    subscribe => File["${certdir}/nss-password.txt"],
-    require   => [
-      Nsstools::Create[$certdir],
-      Class['nsstools'],
-    ],
+  # the exec type in older versions of puppet don't support the umask param
+  if versioncmp($::puppetversion, '3.4.0') >= 0 {
+    exec {"generate_pkcs12_${title}":
+      command   => "/usr/bin/openssl pkcs12 -export -in ${cert} -inkey ${key} -password 'file:${certdir}/nss-password.txt' -out '${certdir}/${pkcs12_name}' -name '${nickname}'",
+      creates   => "${certdir}/${pkcs12_name}",
+      subscribe => File["${certdir}/nss-password.txt"],
+      umask     => '7077',
+      require   => [
+        Nsstools::Create[$certdir],
+        Class['nsstools'],
+      ],
+    }
+  } else {
+    exec {"generate_pkcs12_${title}":
+      command   => "/usr/bin/openssl pkcs12 -export -in ${cert} -inkey ${key} -password 'file:${certdir}/nss-password.txt' -out '${certdir}/${pkcs12_name}' -name '${nickname}'",
+      creates   => "${certdir}/${pkcs12_name}",
+      subscribe => File["${certdir}/nss-password.txt"],
+      require   => [
+        Nsstools::Create[$certdir],
+        Class['nsstools'],
+      ],
+    }
   }
 
   exec { "add_pkcs12_${title}":
